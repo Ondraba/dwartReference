d9f3e1367fee3bede84614fd4303c4909f2a9e02
Ondraba <ratpile@gmail.com>
2017-11-29 23:51:36 +0100

    selects preparation

--- client/components/Tag.js
+++ client/components/Tag.js
@@ -11,11 +11,11 @@ const style = {
   box: {
     paddingTop: 15,
     textAlign: 'center',
-    backgroundColor: 'grey',
+    backgroundColor: '#ff4081',
     maxWidth: 60,
     padding: 5,
     marginLeft: 5,
-    borderRadius: '15%'
+    borderRadius: '2%'
   }
 };
 
