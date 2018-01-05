748d5f992871ef8570cc556521098d55c054df83
Ondraba <ratpile@gmail.com>
2018-01-05 18:49:22 +0100

    assertations

--- client/e2e/add-content-test.js
+++ client/e2e/add-content-test.js
@@ -7,26 +7,13 @@ fixture `Content add test`
 test('New content saved and redirect', async t => {
     const page = new PageObject();
     const contentItemsCount = await page.items.count;
-    const pc = page.items.count;
-    function huhu(){
-        const prom = () => new Promise(function(resolve, reject){
-            try{
-                 resolve(contentItemsCount)
-            }
-            catch(e){
-                 reject(e)   
-            }
-       })
-        return prom()
-    }
-    
-
+    console.log(contentItemsCount)
     await t
         .click(page.submitContent)
 
     await t
         .expect(page.addWrapper.id).notEql('detailPaper')
-        .expect(page.items.count).eql(huhu())
+        .expect(page.items.count).eql(contentItemsCount)
 });
 
 
