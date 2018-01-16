b5530c208135767fdd22593ef63e559596bb1829
Ondraba <ratpile@gmail.com>
2018-01-16 22:12:53 +0100

    add mysql connections

--- server/models/edee.js
+++ server/models/edee.js
@@ -1,3 +1,23 @@
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'edee'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM SOURCE', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.end();
+
+
+
 // var oracledb = require('oracledb');
 
 // var final = 'test'
